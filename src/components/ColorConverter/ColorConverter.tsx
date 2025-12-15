622b6a82b44b1ccb39430f8b81ce2fbddeead7fd
Al101020 <alex.vl.m@yandex.ru>
2025-12-15 15:51:51 +1000

    commit-3

--- src/components/ColorConverter/ColorConverter.tsx
+++ src/components/ColorConverter/ColorConverter.tsx
@@ -1,21 +1,44 @@
 import "./ColorConverter.css";
+import React,{ useState } from "react";
 
 export const ColorConverter = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // console.log("Event type:", e.type);    
-    console.log(e.target);
-    // const eTarget = e.target as HTMLInputElement;
-    // const valueColor = eTarget.childElementCount;
-    // console.log(valueColor);
-    // const valueColo = eTarget;
-    // console.log(valueColo);
+  const [value, setValue] = useState('#');
+  const [rgb, setRgb] = useState('Ошибка!!!');
+
+  const regexp = /#[a-f0-9]{6}/gi;
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();    // console.log("Event type:", e.type);    // console.log(e);
+
+    const input = e.target.value;    // console.log(input);
+
+    
+
+    if (input.match(regexp) !== null) {
+      console.log('(regexp) !== null'); 
+      setValue(input);
+      setRgb(input);
+    } else {
+      console.log('Ошибка!!!');
+      setValue(input);
+      setRgb('Ошибка!!!');
+    }
+
+        
+    // console.log({value});
+ 
   };
 
   return (
-    <form className="colorConverter" autoComplete="off" onChange={handleSubmit}>
-      <input type="text" id="colorHex" name="colorHex" />
-      <div id="colorRGB"></div>
+    <form className="colorConverter" autoComplete="off">
+      <input 
+      type="text" 
+      id="colorHex" 
+      name="colorHex"
+      maxLength={7} 
+      value={value}
+      onChange={handleChange} />
+      <div id="colorRGB">{rgb}</div>
     </form>
   );
 };
